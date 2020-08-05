aefc446ffcf7b8cca7a0e0611444ac3a0f387f49
chuhx <hx.chu@clustar.ai>
2020-08-05 15:16:50 +0800

    feat: add components

---
+++ src/temp.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        temp
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'temp',
+
+    components: { },
+
+    props: { },
+
+    data () {
+        return {
+
+        }
+    },
+
+    computed: { },
+
+    watch: { },
+
+    beforeCreate () {},
+
+    create () {},
+
+    beforeMount () {},
+
+    mounted () {},
+
+    beforeUpdate () {},
+
+    updated () {},
+
+    activated () {},
+
+    deactivated () {},
+
+    beforeDestroy () {},
+
+    destroyed () {},
+
+    methods: {},
+
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>

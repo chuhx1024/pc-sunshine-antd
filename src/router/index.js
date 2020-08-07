0edb16ef6a7215d30cc3fb9d4698a251ba994565
chuhx <hx.chu@clustar.ai>
2020-08-07 15:49:33 +0800

    feat: breadcrumb link done

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,12 @@ import Router from 'vue-router'
 
 import Layout from '@/layout'
 
+// 修复面包屑 路由重复的报错
+const originalPush = Router.prototype.push
+Router.prototype.push = function push (location) {
+    return originalPush.call(this, location).catch(err => err)
+}
+
 Vue.use(Router)
 
 export const constantRoutes = [
